0e53e2c88ca3d6911cd891cd585e06877d49aa70
hirota <hirota.tomohisa@gmail.com>
2023-11-21 10:25:20 +0900

    Completed to modify ASK and BID price

--- simulation.py
+++ simulation.py
@@ -20,34 +20,37 @@ class Broker():
             print("")
             Broker.market_ls[i].show_info()    
             while True:
-                ans=input("Input command!\nNot order:0\tBID:1\tASK:2\n")
+                ans=input("Input command!\nNot order:0\tBID ORDER:1\tASK ORDER:2\n")
                 if ans =="1" or ans=="2":
                     print("OK")
                     break
                 else:
                     print("Again! Please input 0,1 or 2")
+
 class Market():
     currency = "USD/JPY"
     id = 0
-    rate = 150
+    ttm = 150
     def __init__(self,dt):
         self.id=Market.id
         Market.id += 1
         self.dt = dt
-        self.fixed_upward_rate()
-        #self.set_fixed_rate()
-        #self.set_floating_rate()
-    def fixed_upward_rate(self):
+        self.fixed_upward_ttm()
+        #self.set_fixed_ttm()
+        #self.set_floating_ttm()
+        self.bid_price = self.ttm - Broker.spread/2
+        self.ask_price = self.ttm + Broker.spread/2
+    def fixed_upward_ttm(self):
         self.pip=5
-        self.rate = Market.rate
-        Market.rate = round(self.rate+self.pip/100,2)
-    def set_fixed_rate(self):
-        self.rate = Market.rate
-    def set_floating_rate(self):
-        self.rate = round(Market.rate + random.randint(-100,100)/100,2)
-        Market.rate = self.rate
+        self.ttm = Market.ttm
+        Market.ttm = round(self.ttm+self.pip/100,2)
+    def set_fixed_ttm(self):
+        self.ttm = Market.ttm
+    def set_floating_ttm(self):
+        self.ttm = round(Market.ttm + random.randint(-100,100)/100,2)
+        Market.ttm = self.ttm
     def show_info(self):
-        print("ID:",self.id,"\t",self.dt,"\nBID:",self.rate-Broker.spread/2,"\tASK:",self.rate+Broker.spread/2)
+        print("ID:",self.id,"\t",self.dt,"\nBID:",self.bid_price,"\tASK:",self.ask_price)
 
 class Trade():
     pass
